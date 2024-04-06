bd7ed7904a000dcd8c9f8489d03377fa80840750
ashishfargade <ashish.fargade21@vit.edu>
2024-04-06 19:01:00 +0530

    removed bugs lol

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,36 @@
-const express = require('express');
+const express = require("express");
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route GET api/profile
-// @desc TEST route
+// @desc Get logged user profile
 // @access Public
-router.get('/', (req, res) => {
-    res.send('Profile Route');
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if(!profile){
+        return res.status(400).json({message: 'No Profile for current user'});
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST api/profile
+// @desc Fill user profile
+// @access Public
+router.post("/", [auth, [
+
+]], (req, res) => {
+    
 });
 
 module.exports = router;
